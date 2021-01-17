0cbe29d4f0e291ebb87f6accbcdfeba394bc8258
Brett B <65505937+BreadBooks@users.noreply.github.com>
2021-01-16 22:18:50 -0600

    deploying

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ mongoose.connect("mongodb://localhost/budget-tracker",
     }
 );
 
+
 app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
